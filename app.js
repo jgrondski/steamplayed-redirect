412536dcbd71df56cf28b39f641ed4184549e66e
jgrondski <jgrondski@gmail.com>
2019-06-30 01:09:51 -0500

    chore: added redirect stuff

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const compression = require('compression')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const app = express()
 const router = express.Router()
-
-app.set('view engine', 'pug')
+const passport = require('passport');
+const SteamStrategy = require('passport-steam');
 
 if (process.env.NODE_ENV === 'test') {
   // NOTE: aws-serverless-express uses this app for its integration tests
@@ -23,9 +23,6 @@ router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(awsServerlessExpressMiddleware.eventContext())
 
-// NOTE: tests can't find the views directory without this
-app.set('views', path.join(__dirname, 'views'))
-
 router.get('/', (req, res) => {
   res.render('index', {
     apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
@@ -36,57 +33,79 @@ router.get('/sam', (req, res) => {
   res.sendFile(`${__dirname}/sam-logo.png`)
 })
 
-router.get('/users', (req, res) => {
-  res.json(users)
-})
+// GET /auth/steam
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Steam authentication will involve redirecting
+//   the user to steamcommunity.com.  After authenticating, Steam will redirect the
+//   user back to this application at /auth/steam/return
+router.get('/steam',
+  passport.authenticate('steam', { failureRedirect: '/' }),
+  (res) => {
+    console.log('auth/steam...')
+    res.redirect('/');
+  });
 
-router.get('/users/:userId', (req, res) => {
-  const user = getUser(req.params.userId)
+// GET /auth/steam/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+router.get('/steam/return',
+  // Issue #37 - Workaround for Express router module stripping the full url, causing assertion to fail 
+  (req, res, next) => {
+      console.log('auth/steam/return...')
+      req.url = req.originalUrl;
+      next();
+  }, 
+  passport.authenticate('steam', { failureRedirect: '/' }),
+    (req, res) => {
+        console.log('auth/steam/return authenticate callback...');
+        console.log(req.user.id);
+        res.redirect('http://localhost:3000/userid/' + req.user.id);
+    }
+);
 
-  if (!user) return res.status(404).json({})
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Steam profile is serialized.
+passport.serializeUser((user, done) => {
+  console.log('serializeUser...');
+  done(null, user);
+});
 
-  return res.json(user)
-})
+// Use the SteamStrategy within Passport.
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, an OpenID identifier and profile), and invoke a
+//   callback with a user object.
+passport.use(new SteamStrategy({
+    providerUrl: 'https://cors-anywhere.herokuapp.com/https://steamcommunity.com/openid',
+    returnURL: 'http://localhost:3001/auth/steam/return',
+    realm: 'http://localhost:3001/',
+    apiKey: '7813876CA4146DF8758A701FAF9C9A99'
+  },
+  (identifier, profile, done) => {
+    console.log('passport.use...')
+    // asynchronous verification, for effect...
+    process.nextTick(() => {
 
-router.post('/users', (req, res) => {
-  const user = {
-    id: ++userIdCounter,
-    name: req.body.name
+      // To keep the example simple, the user's Steam profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Steam account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
   }
-  users.push(user)
-  res.status(201).json(user)
-})
+));
 
-router.put('/users/:userId', (req, res) => {
-  const user = getUser(req.params.userId)
+// Initialize Passport
+app.use(passport.initialize());
 
-  if (!user) return res.status(404).json({})
-
-  user.name = req.body.name
-  res.json(user)
-})
-
-router.delete('/users/:userId', (req, res) => {
-  const userIndex = getUserIndex(req.params.userId)
-
-  if (userIndex === -1) return res.status(404).json({})
-
-  users.splice(userIndex, 1)
-  res.json(users)
-})
-
-const getUser = (userId) => users.find(u => u.id === parseInt(userId))
-const getUserIndex = (userId) => users.findIndex(u => u.id === parseInt(userId))
-
-// Ephemeral in-memory data store
-const users = [{
-  id: 1,
-  name: 'Joe'
-}, {
-  id: 2,
-  name: 'Jane'
-}]
-let userIdCounter = users.length
+// Tell the app to use router for anything that uses /auth
+app.use('/auth', router);
 
 // The aws-serverless-express library creates a server and listens on a Unix
 // Domain Socket for you, so you can remove the usual call to app.listen.
