68b324736678a7c7e1cd219766a9979289700c88
Josh Grondski <jgrondski@gmail.com>
2019-06-30 01:18:53 -0500

    chore: command stuff

--- app.local.js
+++ app.local.js
@@ -3,3 +3,5 @@ const port = 3001
 
 app.listen(port)
 console.log(`listening on http://localhost:${port}`)
+
+// npm run config -- --account-id="835963302541" --bucket-name="redirect.steamplayed.com" --function-name="SteamPlayedRedirectFunction"
